eec12974d342e7e44d11b316439ae769311896ee
Twigs <kimorvenvalencia@gmail.com>
2025-03-13 14:57:48 +0800

    setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document</title>
+	</head>
+	<?php 
+	include './js_php/tailwind_config.html'; 
+	?>
+	<body>
+		<div class="flex flex-row min-h-screen justify-center items-center font-neues-grotesque">
+			This text is using Neues Grotesque!
+		</div>
+	</body>
+</html>
