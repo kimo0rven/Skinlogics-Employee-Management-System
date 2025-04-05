ee5a43777e627a7b15d586effc79b4e02291c3f3
Twigs <kimorvenvalencia@gmail.com>
2025-04-05 22:04:41 +0800

    update

--- newlogin.php
+++ newlogin.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style.css" />
     <title>Messenger Login</title>
 </head>
+
 <body>
-    <div class=" login-container"> 
-    <div class="login-container-div">
-        <div class="text-section">
-            <h1 class="text-section-title" >A place for meaningful conversations</h1>
-            <p class="subtext">Messenger helps you connect with your Facebook friends and family, build your community, and deepen your interests.</p>
-            <form class="login-form">
-                <input class="login-input" type="text" placeholder="Email or phone number" required>
-                <input class="login-input" type="password" placeholder="Password" required>
-                <div class="login-button-container">
-                    <button class="login-button" type="submit">Log in</button>
-                    <a href="#" class="forgot-password">Forgotten your password?</a>
+    <div class=" login-container">
+        <div class="login-container-div">
+            <div class="text-section">
+                <h1 class="text-section-title">A place for meaningful conversations</h1>
+                <p class="subtext">Messenger helps you connect with your Facebook friends and family, build your
+                    community, and deepen your interests.</p>
+                <form class="login-form">
+                    <input class="login-input" type="text" placeholder="Email or phone number" required>
+                    <input class="login-input" type="password" placeholder="Password" required>
+                    <div class="login-button-container">
+                        <button class="login-button" type="submit">Log in</button>
+                        <a href="#" class="forgot-password">Forgotten your password?</a>
+                    </div>
+                </form>
+                <div class="alt-login">
+                    <img src="microsoft.jpg" alt="Get it from Microsoft">
+                    <img src="google.jpg" alt="Google Login">
                 </div>
-            </form>
-            <div class="alt-login">
-                <img src="microsoft.jpg" alt="Get it from Microsoft">
-                <img src="google.jpg" alt="Google Login">
+            </div>
+            <div class="mockup-section">
+                <img src="messenger.png" alt="Messenger Mockup">
             </div>
         </div>
-        <div class="mockup-section">
-            <img src="messenger.png" alt="Messenger Mockup">
-        </div>
-    </div> 
     </div>
 </body>
-</html>  
+
+</html>
